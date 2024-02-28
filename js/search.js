7fdfbd6c7f8078c31ba1d5a5a0a5a5270f7af3dd
bigdata <tnals2199@naver.com>
2024-02-29 01:21:27 +0900

    병합 전 저장

---
+++ js/search.js
@@ -0,0 +1,21 @@
+let searchInput = document.getElementById("search-input");
+console.log(searchInput)
+
+// searchInput.addEventListener("click", () => {
+
+// })
+
+// const modalButton = document.getElementById("search-input");
+// const modal = document.querySelector(".modal");
+// const closeButton = document.getElementById("closeButton");
+// const modal_overlay = modal.querySelector(".modal_overlay");
+
+// modalButton.addEventListener("focus", () => {
+//     modal.classList.remove("hidden");
+// })
+// closeButton.addEventListener("click", () => {
+//     modal.classList.add("hidden");
+// })
+// modal_overlay.addEventListener("click", () => {
+//     modal.classList.add("hidden");
+// })
