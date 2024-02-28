167386bbb0a39537f2681506d075ddc6dedf836e
dongcheon1020 <dongcheon1020@gmail.com>
2024-02-28 22:44:49 +0900

    feat:add  booklist api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,51 @@
-const API_KEY =
-  "ddad4259b659af428252ec826266babcb91d3bedc9d03f0dc7c703a28c0100b3";
+const API_KEY = `ddad4259b659af428252ec826266babcb91d3bedc9d03f0dc7c703a28c0100b3`;
+let booklistAll = [];
+let pageSize = 5;
+let pageNo = 1;
+const booklistBts = document.querySelectorAll(".booklist-bt");
 
+booklistBts.forEach((e) => {
+  e.addEventListener("click", () => {
+    console.log(e.id);
+  });
+});
 const getAPI = async () => {
   const url = new URL(
-    `https://www.nl.go.kr/NL/search/openApi/search.do?key=${API_KEY}&result_style=json`
+    `https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${API_KEY}&result_style=json`
   );
-  console.log(11);
-  const response = await fetch(url);
-  console.log("response", response);
+  url.searchParams.set("page_size", pageSize);
+  url.searchParams.set("page_no", pageNo);
 
-  //   const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
+  booklistAll = data.docs;
+  rander();
 };
 getAPI();
+
+const rander = () => {
+  let bookListAllHTML = booklistAll
+    .map(
+      (i) => `
+    <li class="booklist-item">
+        <div class="booklist-img-box">
+            <img class="booklist-img" src="${
+              i.TITLE_URL || "../images/bookpage.svg"
+            }" alt="책 표지" />
+            
+            <span class="booklist-sub-title">${
+              i.TITLE_URL == "" ? i.TITLE : ""
+            }</span>
+            <span class="booklist-sub-author"> ${
+              i.TITLE_URL == "" ? i.AUTHOR : ""
+            }</span>
+        </div>
+        <h3 class="booklist-title">${i.TITLE}</h3>
+        <span class="booklist-author">${i.AUTHOR} 지음</span>
+    </li>
+  `
+    )
+    .join("");
+
+  document.querySelector(".booklist-all").innerHTML = bookListAllHTML;
+};
