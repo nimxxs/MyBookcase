b92a112c67521a8dd58e2dd0f734809678120c80
choisojin <choisojin@naver.com>
2024-02-28 14:59:19 +0900

    리스트-이미지있는경우만

--- js/list.js
+++ js/list.js
@@ -2,7 +2,7 @@ const API_KEY = '1fcc678ac940549cb24a61ded5ec9453a2924d7475da7cb94de1d5ad53ee821
 let newsList=[]
 
 const getLibrary = async () => {
-    const url = new URL(`https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${API_KEY}&result_style=json&page_no=1&page_size=10&start_publish_date=20220509&end_publish_date=20220509`)
+    const url = new URL(`https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${API_KEY}&result_style=json&page_no=1&page_size=100&start_publish_date=20220509&end_publish_date=20220509`)
     //console.log("url", url)
     const response = await fetch(url);
     const data = await response.json()
@@ -12,7 +12,9 @@ const getLibrary = async () => {
 }
 
 const render=()=>{
-    const newsHTML = newsList.map(news=>`<div class="row">
+    const newsHTML = newsList
+    .filter(news => news.TITLE_URL)
+    .map(news=>`<div class="row">
     <img src=${news.TITLE_URL}>
     <ol>${news.TITLE}</ol>
     <ul>${news.AUTHOR}</ul>
