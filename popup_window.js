9b5651ccc9777f2ee1519eea01d959016e41fcb3
emeryss <emeryss.steam@gmail.com>
2024-02-28 20:05:09 +0900

    still testing

---
+++ popup_window.js
@@ -0,0 +1,22 @@
+// open new window for detailed info
+function popWindow() {
+    let params = `
+        scrollbars=yes,
+        resizable=yes,
+        status=no,
+        location=no,
+        toolbar=no,
+        menubar=yes,
+        width=1000,
+        height=800,
+        left=(window.screen.width / 2) - (width/2),
+        top=(window.screen.height / 4)
+        `;
+    window.open("detail_page.html", "a", params); 
+}
+
+// This event handler will listen for messages from the child
+window.addEventListener("message", (e)=>{
+    //e.data hold the message from the child
+    console.log(e.data)
+})
