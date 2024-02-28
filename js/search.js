726dd740ecf1fab46708e795145654cf8c647216
dongcheon1020 <dongcheon1020@gmail.com>
2024-02-29 02:51:47 +0900

    fix:booklist

--- js/search.js
+++ js/search.js
@@ -1,15 +1,15 @@
-let searchInput = document.getElementById("search-input");
-searchInput.addEventListener("click", () => {
-  searchInput.style.border = "2px solid #fff";
-  searchInput.style.boxShadow = "0px 0px 12px rgba(255, 255, 255, 0.2)";
-  searchInput.style.backgroundColor = "#ffffff40";
-});
-searchInput.addEventListener("blur", () => {
-  searchInput.style.border = "none";
-  searchInput.style.boxShadow = "none";
-  searchInput.style.backgroundColor = "#ffffff6c";
-  searchInput.value = "";
-});
+// let searchInput = document.getElementById("search-input");
+// searchInput.addEventListener("click", () => {
+//   searchInput.style.border = "2px solid #fff";
+//   searchInput.style.boxShadow = "0px 0px 12px rgba(255, 255, 255, 0.2)";
+//   searchInput.style.backgroundColor = "#ffffff40";
+// });
+// searchInput.addEventListener("blur", () => {
+//   searchInput.style.border = "none";
+//   searchInput.style.boxShadow = "none";
+//   searchInput.style.backgroundColor = "#ffffff6c";
+//   searchInput.value = "";
+// });
 // searchInput.addEventListener("click", () => {
 
 // })
