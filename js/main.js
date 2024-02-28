aee62b55241addb63599f8bc065deea6c9566d46
dongcheon1020 <dongcheon1020@gmail.com>
2024-02-29 01:54:07 +0900

    fix: test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  // 여기에 DOM 요소를 참조하는 코드를 넣으면 됩니다.
-  let myElement = document.querySelector(".booklist-all");
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   // 여기에 DOM 요소를 참조하는 코드를 넣으면 됩니다.
+//   let myElement = document.querySelector(".booklist-all");
+// });
 
+const bookListHTML = ocument.querySelector(".booklist-all");
 const API_KEYdong = `ddad4259b659af428252ec826266babcb91d3bedc9d03f0dc7c703a28c0100b3`;
 let booklistAll = [];
 let pageSize = 5;
@@ -64,7 +65,7 @@ const rander = () => {
   `
     )
     .join("");
-  document.querySelector(".booklist-all").innerHTML = bookListAllHTML;
+  bookListHTML.innerHTML = bookListAllHTML;
 };
 // 버튼 클릭시 북리스트 다음페이지로 넘김
 booklistBts.forEach((e) => {
