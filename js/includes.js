8e0391d00759b10e0df72ad432074d1ce3c9c3a6
bigdata <tnals2199@naver.com>
2024-03-01 02:34:12 +0900

    파일 병합

--- js/includes.js
+++ js/includes.js
@@ -1,32 +1,61 @@
-// [참고] https://www.w3schools.com/howto/howto_html_include.asp
+// // [참고] https://www.w3schools.com/howto/howto_html_include.asp
 
-function includeHTML(callback = function() {}) {
-    let z, i, elmnt, file, xhr;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      file = elmnt.getAttribute("include-html");
-      if (file) {
-        xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {
-              elmnt.innerHTML = this.responseText;
-              callback(); //바꾼거
-            }
-            if (this.status == 404) {
-              elmnt.innerHTML = "Page not found.";
-            }
-            elmnt.removeAttribute("include-html");
-            includeHTML(callback); 
+// function includeHTML(callback = function () {}) {
+//   let z, i, elmnt, file, xhr;
+//   z = document.getElementsByTagName("*");
+//   for (i = 0; i < z.length; i++) {
+//     elmnt = z[i];
+//     file = elmnt.getAttribute("include-html");
+//     if (file) {
+//       xhr = new XMLHttpRequest();
+//       xhr.onreadystatechange = function () {
+//         if (this.readyState == 4) {
+//           if (this.status == 200) {
+//             elmnt.innerHTML = this.responseText;
+//           }
+//           if (this.status == 404) {
+//             elmnt.innerHTML = "Page not found.";
+//           }
+//           elmnt.removeAttribute("include-html");
+//           includeHTML(callback);
+//         }
+//       };
+//       xhr.open("GET", file, true);
+//       xhr.send();
+//       return;
+//     }
+//   }
+//   setTimeout(function () {
+//     callback();
+//   }, 0);
+// }
+
+// console.log(22);
+function includeHTML(callback) {
+  callback = callback || function () {}; // callback이 undefined일 경우, 빈 함수를 할당
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
           }
-        };
-        xhr.open("GET", file, true);
-        xhr.send();
-        return;
-      }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          elmnt.removeAttribute("include-html");
+          includeHTML(callback); // 콜백 함수를 인자로 전달
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
     }
-    setTimeout(function() {
-      callback();
-    }, 0);
+  }
+  setTimeout(() => callback(), 0);
 }
--- js/myBooklist.js
+++ js/myBooklist.js
@@ -1,17 +1,37 @@
-const myAPI = '7a328df4e2e5bc6c3dd52d51251e3469a5d0910058d86c71204180d55a9afce2'
-let ISBNUrl = new URL(`https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${myAPI}&result_style=json&page_no=1&page_size=1`)
-let RecommendUrl = new URL(`https://corsproxy.io/?https://nl.go.kr/NL/search/openApi/saseoApi.do?key=${myAPI}&endRowNemApi=100&start_date=20220101`)
-let bookList = []
-let matchArray = []
-let matchBookList = []
+const myAPI =
+  "7a328df4e2e5bc6c3dd52d51251e3469a5d0910058d86c71204180d55a9afce2";
+let ISBNUrl = new URL(
+  `https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${myAPI}&result_style=json&page_no=1&page_size=1`
+);
+let RecommendUrl = new URL(
+  `https://corsproxy.io/?https://nl.go.kr/NL/search/openApi/saseoApi.do?key=${myAPI}&endRowNemApi=100&start_date=20220101`
+);
+let bookList = [];
+let matchArray = [];
+let matchBookList = [];
 
 let ISBNList = [
-    { isbn: "9791185035154", url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1267" },
-    { isbn: "9788961961844", url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1259" },
-    { isbn: "9791167373618", url: "https://www.nl.go.kr/afile/previewThumbnail/24013052262qnXFI" },
-    { isbn: '9788983717054', url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1278" },
-    { isbn: '9788996586043', url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1056" }
-]
+  {
+    isbn: "9791185035154",
+    url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1267",
+  },
+  {
+    isbn: "9788961961844",
+    url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1259",
+  },
+  {
+    isbn: "9791167373618",
+    url: "https://www.nl.go.kr/afile/previewThumbnail/24013052262qnXFI",
+  },
+  {
+    isbn: "9788983717054",
+    url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1278",
+  },
+  {
+    isbn: "9788996586043",
+    url: "https://www.nl.go.kr/afile/previewThumbnail/NLR-1056",
+  },
+];
 
 // let ISBNList = ["9791185035154", "9788961961844", "9791167373618", '9788983717054', '9788996586043']
 
@@ -72,55 +92,57 @@ let ISBNList = [
 //     render();
 // }
 
-
 // recommend()
 
 // xml json으로 변환 함수
 function xmlToJson(xml) {
-    // Create the return object
-    var obj = {};
+  // Create the return object
+  var obj = {};
 
-    if (xml.nodeType == 1) { // element node
-        // do attributes
-        if (xml.attributes.length > 0) {
-            obj["@attributes"] = {};
-            for (var j = 0; j < xml.attributes.length; j++) {
-                var attribute = xml.attributes.item(j);
-                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-            }
-        }
-    } else if (xml.nodeType == 3) { // text node
-        obj = xml.nodeValue;
+  if (xml.nodeType == 1) {
+    // element node
+    // do attributes
+    if (xml.attributes.length > 0) {
+      obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
     }
+  } else if (xml.nodeType == 3) {
+    // text node
+    obj = xml.nodeValue;
+  }
 
-    // do children
-    if (xml.hasChildNodes()) {
-        for (var i = 0; i < xml.childNodes.length; i++) {
-            var item = xml.childNodes.item(i);
-            var nodeName = item.nodeName;
-            if (typeof obj[nodeName] == "undefined") {
-                obj[nodeName] = xmlToJson(item);
-            } else {
-                if (typeof obj[nodeName].push == "undefined") {
-                    var old = obj[nodeName];
-                    obj[nodeName] = [];
-                    obj[nodeName].push(old);
-                }
-                obj[nodeName].push(xmlToJson(item));
-            }
+  // do children
+  if (xml.hasChildNodes()) {
+    for (var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof obj[nodeName] == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof obj[nodeName].push == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
         }
+        obj[nodeName].push(xmlToJson(item));
+      }
     }
-    return obj;
+  }
+  return obj;
 }
 
 const render = () => {
-    const bookHTML = matchBookList.map((book) => {
-        // ISBNList에서 해당 도서의 ISBN에 해당하는 URL을 찾습니다.
-        const isbnItem = ISBNList.find(item => item.isbn === book.EA_ISBN);
-        // ISBN에 해당하는 URL이 존재한다면 이미지 소스로 설정합니다.
-        const imageURL = isbnItem ? isbnItem.url : '';
+  const bookHTML = matchBookList
+    .map((book) => {
+      // ISBNList에서 해당 도서의 ISBN에 해당하는 URL을 찾습니다.
+      const isbnItem = ISBNList.find((item) => item.isbn === book.EA_ISBN);
+      // ISBN에 해당하는 URL이 존재한다면 이미지 소스로 설정합니다.
+      const imageURL = isbnItem ? isbnItem.url : "";
 
-        return `
+      return `
         <div class="book">
             <div class="book_firstDiv">
                 <input type="checkbox" id="buy">
@@ -139,33 +161,35 @@ const render = () => {
                 </button>
             </div>
         </div>
-        `
-    }).join("")
-    document.querySelector(".booksArea").innerHTML = bookHTML
-}
+        `;
+    })
+    .join("");
+  document.querySelector(".booksArea").innerHTML = bookHTML;
+};
 
 async function matchISBN() {
-    if (!Array.isArray(ISBNList)) {
-        console.error('ISBNList is not an array');
-        return;
-    }
+  if (!Array.isArray(ISBNList)) {
+    console.error("ISBNList is not an array");
+    return;
+  }
 
-    for (let i of ISBNList) {
-        ISBNUrl.searchParams.set('isbn', i.isbn)
-        console.log("ISBNUrl.href: ", ISBNUrl.href)
-        const response = await fetch(ISBNUrl.href)
-        const matchData = await response.json()
-        matchArray.push(matchData)
+  for (let i of ISBNList) {
+    ISBNUrl.searchParams.set("isbn", i.isbn);
+    console.log("ISBNUrl.href: ", ISBNUrl.href);
+    const response = await fetch(ISBNUrl.href);
+    const matchData = await response.json();
 
-        console.log("매칭된 데이터: ", matchData)
-    }
-    console.log("matchArray: ", matchArray)
+    matchArray.push(matchData);
 
-    matchBookList = matchArray.flatMap(item => item.docs || []);
+    console.log("매칭된 데이터: ", matchData);
+  }
+  console.log("matchArray: ", matchArray);
 
-    console.log("matchBookList: ", matchBookList)
+  matchBookList = matchArray.flatMap((item) => item.docs || []);
 
-    render()
+  console.log("matchBookList: ", matchBookList);
+
+  render();
 }
 
-matchISBN()
+matchISBN();
