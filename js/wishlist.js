ba2e65589728d8d7e67efc8793c47b6c88413d5f
soomin <tnals2199@naver.com>
2024-03-03 19:22:48 +0900

    main.css 수정

--- js/wishlist.js
+++ js/wishlist.js
@@ -8,7 +8,8 @@ const groupSize2 = 5;
 const category = 0;
 
 const getLibrary = async () => {
-    const url = new URL(`https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${API_KEY2}&result_style=json&page_no=${page2}&page_size=${pageSize2}`); //isbn    
+    // const url = new URL(`https://www.nl.go.kr/seoji/SearchApi.do?cert_key=${API_KEY2}&result_style=json&page_no=${page2}&page_size=${pageSize2}`); //isbn    
+    const url = new URL(`https://corsproxy.io/?https://nl.go.kr/NL/search/openApi/saseoApi.do?key=${API_KEY}&startRowNumApi=1&endRowNumApi=1325`); //사서추천    
     
     const response = await fetch(url);    
     const data = await response.json();
@@ -18,6 +19,30 @@ const getLibrary = async () => {
     paginationRender2();
 };
 
+// const render = () => {
+//     const newsHTML = newsList.slice(0, 20) // 최대 20개 항목까지만 출력
+//         .map(news => {
+//             // 입력된 문자열을 "YYYY.MM.DD" 형식으로 변환
+//             const dateString = news.PUBLISH_PREDATE.slice(0, 4) + '.' + news.PUBLISH_PREDATE.slice(4, 6) + '.' + news.PUBLISH_PREDATE.slice(6, 8);
+            
+//             return `
+//                 <section>
+//                     <div><img class="booklist-img" src="${news.imageUrl || "../images/bookskin.png"}" alt="책 표지" /></div>
+//                     <div>
+//                         <ol>${news.TITLE}</ol>
+//                         <ul>${news.AUTHOR}</ul>
+//                         <ul>${news.PUBLISHER}</ul>
+//                         <ul>${dateString}</ul> <!-- 변환된 날짜 문자열 사용 -->
+//                         <div class="price-style">${news.PRE_PRICE}</div>
+//                     </div>        
+//                 </section>
+//                 <div class="list-contents-border"></div>`;
+//         }).join('');
+            
+//     document.getElementById("news-board").innerHTML = newsHTML;
+// };
+
+// 사서 추천 render
 const render = () => {
     const newsHTML = newsList.slice(0, 20) // 최대 20개 항목까지만 출력
         .map(news => {
@@ -26,11 +51,11 @@ const render = () => {
             
             return `
                 <section>
-                    <div><img class="booklist-img" src="${news.imageUrl || "../images/bookskin.png"}" alt="책 표지" /></div>
+                    <div><img class="booklist-img" src="${news.item.recomisbn["#text"] || "../images/bookskin.png"}" alt="책 표지" /></div>
                     <div>
-                        <ol>${news.TITLE}</ol>
-                        <ul>${news.AUTHOR}</ul>
-                        <ul>${news.PUBLISHER}</ul>
+                        <ol>${news.item.recomtitle["#text"]}</ol>
+                        <ul>${news.item.recomauthor["#text"]}</ul>
+                        <ul>${news.item.recompublisher["#text"]}</ul>
                         <ul>${dateString}</ul> <!-- 변환된 날짜 문자열 사용 -->
                         <div class="price-style">${news.PRE_PRICE}</div>
                     </div>        
@@ -85,10 +110,10 @@ const nextToPage2 = () => {
     }
 };
 
-const getCategory = async (category) => {
-    const encodedCategory = encodeURIComponent(category);
-    const url = `https://www.nl.go.kr/NL/search/openApi/search.do?key=1fcc678ac940549cb24a61ded5ec9453a2924d7475da7cb94de1d5ad53ee8212&kwd=${encodedCategory}`;
-    window.open(url, '_self');
-};
+// const getCategory = async (category) => {
+//     const encodedCategory = encodeURIComponent(category);
+//     const url = `https://www.nl.go.kr/NL/search/openApi/search.do?key=1fcc678ac940549cb24a61ded5ec9453a2924d7475da7cb94de1d5ad53ee8212&kwd=${encodedCategory}`;
+//     window.open(url, '_self');
+// };
 
 getLibrary();
